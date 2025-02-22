293dcb2f4e6cb589e276b73e035207d8b7bd860a
태훈김 <taehungim@taehungims-MacBook-Air.local>
2025-02-23 02:22:47 +0900

    수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,14 @@
-const { CONFIG } = require("./site.config")
-
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: CONFIG.link,
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-  generateIndexSitemap: false,
-}
+  siteUrl: 'https://toritori3434.vercel.app', // 실제 배포된 도메인 입력
+  generateRobotsTxt: true, // robots.txt 자동 생성 활성화
+  sitemapSize: 5000, // 한 파일에 최대 5000개의 URL 포함 (필요에 따라 조정)
+  exclude: ['/admin/*'], // 제외할 경로 (필요에 따라 수정)
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+      { userAgent: 'Googlebot', allow: '/' },
+      { userAgent: 'Bingbot', allow: '/' }
+    ],
+  },
+};
--- next-sitemap.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    siteUrl: 'https://toritori3434.vercel.app', // 🔹 본인 사이트 URL로 변경!
-    generateRobotsTxt: true, // robots.txt도 자동 생성
-    sitemapSize: 5000,
-  };
-  
